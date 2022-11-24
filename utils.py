1e6a086599d39cfc64c9a35d81f69e20b30502a8
Miko <mikolajkacki98@gmail.com>
2022-11-24 20:42:03 +0000

    moving the plotting functions

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import datetime
-import seaborn as sns
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
 
@@ -28,30 +26,3 @@ def process_dates_to_weeks(df):
 
     return df
 
-def plot_day_tweets_count(df):
-    """ Plot number of tweets per day. """
-
-    df = process_dates_to_weeks(df)
-    df = df.groupby(by='Day').agg(lambda x: x.to_list()).reset_index()
-    df['Count'] = df.Date.apply(lambda x: len(x))
-    df = df.sort_values(by='Day')
-
-    fig = plt.figure(figsize=(10, 4))
-    sns.barplot(data = df, x = 'Day', y = 'Count')
-    plt.title("Number of Tweets published in January.")
-    
-    return fig
-
-def plot_day_likes_count(df):
-    """ Plot number of likes the tweets get per day. """
-
-    df = process_dates_to_weeks(df)
-    df = df.groupby(by='Day').agg(lambda x: x.to_list()).reset_index()
-    df['Count'] = df.Likes.apply(lambda x: sum(x))
-    df = df.sort_values(by='Day')
-
-    fig = plt.figure(figsize=(10, 4))
-    sns.barplot(data = df, x = 'Day', y = 'Count')
-    plt.title("Number of likes tweets received in January.")
-    
-    return fig
